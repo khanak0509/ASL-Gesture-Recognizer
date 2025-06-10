e41c613d884ec1a47ce120e07c92d41054e5247c
Khanak <kkhanak512@gmail>
2025-06-10 23:20:58 +0530

    Real time American sign language detection

--- final.py
+++ final.py
@@ -9,7 +9,7 @@ engine = pyttsx3.init()
 engine.setProperty('rate', 170)
 engine.setProperty('volume', 1.0)
 
-from suggesstion import suggestion
+from suggestion import suggestion
 
 
 def speak(text):
